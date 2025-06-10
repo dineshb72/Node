55dd793ad5b9621452a59a0fb9a14b44dd7cdaa9
dineshb72 <bukyadinesh72@gmail.com>
2025-06-10 04:50:53 +0000

    k1

---
+++ portfolio/next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  basePath: "/Node",
+  assetPrefix: "/Node/",
+};
+
+module.exports = nextConfig;
+
+
